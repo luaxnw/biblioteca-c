5cf1900666041c1ae1f57f108c877ce6618a198f
luaxnw <luan.pescador@estudante.uffs.edu.br>
2026-09-23 10:38:55 -0300

    define estrutura de autor no cabeçalho

--- include/autor.h
+++ include/autor.h
@@ -3,8 +3,11 @@
 
 #define SIZE_NOME 30
 
-typedef struct Autor Autor;
-typedef struct Livro Livro;
+typedef struct Autor
+{
+    char nome[SIZE_NOME];
+    ListaLivros *listaDoAutor;
+} Autor;
 
 Autor *criaAutor(char nome[SIZE_NOME]);
 
